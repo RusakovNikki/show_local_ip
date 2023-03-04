ccac191f59894e1c240a38b1c70a97ed4982f38a
RusakovNikki <rusakov.nikki@yandex.ru>
2023-03-05 00:05:47 +0300

    bug fix

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
+import fs from 'fs'
+import { validationResult } from "express-validator";
 import express from 'express'
 import jwt from 'jsonwebtoken'
-import mongoose from 'mongoose'
-import { validationResult } from "express-validator";
+import bcript from 'bcrypt'
 
+import Users from './data/Users.json' assert { type: "json" }
 import { regValidation } from './validations/auth.js'
 
-mongoose
-    .connect('mongodb+srv://admin:admin@cluster0.on4l6vj.mongodb.net/?retryWrites=true&w=majority')
-    .then(() => console.log('DataBase OK'))
-    .catch(err => console.log(err))
 
 const app = express()
-
 app.use(express.json()) // разрешается отправлять данные в формате json на сервер
 
-app.post('/reg', regValidation, (req, res) => {
+app.post('/reg', regValidation, async (req, res) => {
 
     const errors = validationResult(req)
 
@@ -22,10 +19,25 @@ app.post('/reg', regValidation, (req, res) => {
         return res.status(400).json(errors.array())
     }
 
-    res.json({
-        success: true
+    const password = req.body.password
+    const salt = await bcript.genSalt(10)
+    const passwordHash = await bcript.hash(password, salt)
+
+    const user = {
+        email: req.body.email,
+        passwordHash,
+        fullName: req.body.fullName
+    }
+    Users.push(user)
+    console.log(Users);
+    await fs.writeFile('./data/Users.json', JSON.stringify(Users), err => {
+        console.log(err);
     })
+
+    res.json(user)
 })
+
+
 
 app.listen(4444, (err) => {
     if (err) {
